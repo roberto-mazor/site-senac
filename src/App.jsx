16c7a99e7e8d01cff4498b33fd25a11d111311dc
roberto-mazor <mazor.estudo@gmail.com>
2025-07-03 20:12:43 -0300

    finalizado inicio react
    alterando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logosenac from './assets/logo-senac.png'
 import './App.css'
 
 function App() {
@@ -9,24 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a href="https://sp.senac.br/americana" target="_blank">
+          <img src={logosenac} className="logo react" alt="Senac logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Começando com React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador é {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Click na imagem e conheça nossos cursos!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      💧Beba água!💧
       </p>
     </>
   )
